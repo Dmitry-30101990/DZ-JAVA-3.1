d02ab1c3c30a6742f85fb418afa6203327759ab4
Dmitry Zhmakin <dima8837922@gmail.com>
2021-04-18 20:05:52 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	long amount = 2850;
-	int mile = 20;
-	long bonus  = amount/mile;
-	System.out.println(bonus);
-
+        long amount = 2850;
+        int mile = 20;
+        long bonus = amount / mile;
+        System.out.println(bonus);
 
 
     }
